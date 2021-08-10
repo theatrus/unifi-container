a453054cb7160cadc92dedbbd3fac7c253716c2c
Yann Ramin <atrus@stackworks.net>
2021-08-10 15:32:09 -0700

    Sync changes

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
 FROM ubuntu:xenial
 
 RUN apt-get update -y && \
-    apt-get install -y wget curl jsvc openjdk-8-jre-headless mongodb-server binutils libcap2
+    apt-get install -y wget curl jsvc openjdk-8-jre-headless mongodb-server binutils libcap2 logrotate
     
-RUN wget https://dl.ui.com/unifi/5.12.35/unifi_sysvinit_all.deb && \
+RUN wget https://dl.ui.com/unifi/6.2.26/unifi_sysvinit_all.deb && \
     dpkg -i unifi_sysvinit_all.deb && \
     rm unifi_sysvinit_all.deb
 
 COPY run-unifi.sh /run-unifi.sh
 
-EXPOSE 8443
 CMD ["/run-unifi.sh"]
--- run-unifi.sh
+++ run-unifi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JAVA_VARS="-Dunifi.datadir=/var/lib/unifi -Dunifi.logdir=/var/log/unifi -Dunifi.rundir=/var/run/unifi  -Xmx1024M -Djava.awt.headless=true -Dfile.encoding=UTF-8  -XX:+ExitOnOutOfMemoryError -XX:+CrashOnOutOfMemoryError -XX:ErrorFile=/usr/lib/unifi/logs/hs_err_pid%p.log"
+JAVA_VARS="-Djava.net.preferIPv4Stack=true -Dunifi.datadir=/var/lib/unifi -Dunifi.logdir=/var/log/unifi -Dunifi.rundir=/var/run/unifi  -Xmx4096M -Djava.awt.headless=true -Dfile.encoding=UTF-8  -XX:+ExitOnOutOfMemoryError -XX:+CrashOnOutOfMemoryError -XX:ErrorFile=/usr/lib/unifi/logs/hs_err_pid%p.log"
 JVM="/usr/lib/jvm/java-8-openjdk-amd64/bin/java"
 
 exec "${JVM}" ${JAVA_VARS}  -jar /usr/lib/unifi/lib/ace.jar start
